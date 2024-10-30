2dddeb373d9c251e6a2e890c428f3aaec76e52f7
SenorPez <senorpez@senorpez.com>
2024-10-30 17:50:31 -0500

    Refactored in preparation for window resizing.

--- navigator/src/app/star-map/star-map.component.ts
+++ navigator/src/app/star-map/star-map.component.ts
@@ -1,8 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as THREE from 'three';
-import {Camera, MeshBasicMaterial} from 'three';
+import {Camera, MeshBasicMaterial, WebGLRenderer} from 'three';
 import {StarSystemService} from "../star-system.service";
-import {StarSystem} from "../star-system";
 import {TrackballControls} from "three/examples/jsm/controls/TrackballControls";
 
 @Component({
@@ -12,35 +11,120 @@ import {TrackballControls} from "three/examples/jsm/controls/TrackballControls";
   templateUrl: './star-map.component.html',
   styleUrl: './star-map.component.css'
 })
-export class StarMapComponent {
-  scene = new THREE.Scene();
+export class StarMapComponent implements OnInit {
+  scene;
   camera;
-  renderer = new THREE.WebGLRenderer();
-
-  controls: TrackballControls;
+  renderer;
+  controls;
 
   animate = () => {
     this.controls.update();
     this.renderer.render(this.scene, this.camera);
   }
 
-  createControls(camera: Camera) {
-    return new TrackballControls(camera, this.renderer.domElement);
-  }
-
-  getLimit = (starSystems: StarSystem[]) => {
-    return Math.max(
-      Math.max(...starSystems.map(star => Math.abs(star.coordinates.x))),
-      Math.max(...starSystems.map(star => Math.abs(star.coordinates.y))),
-      Math.max(...starSystems.map(star => Math.abs(star.coordinates.z)))
-    )
-  }
-
   constructor(private starSystemsService: StarSystemService) {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);
+    this.scene = this.createScene();
+    this.camera = this.createCamera(this.getCoordinateLimit());
+    this.renderer = this.createRenderer();
+    this.controls = this.createControls(this.camera, this.renderer);
+  }
 
-    const coordinateLimit = this.getLimit(starSystemsService.getStarSystems()) * 1.05;
+  ngOnInit() {
+    this.camera.position.z = this.getCoordinateLimit() * 2;
+
+    this.addStars(this.scene);
+    this.addJumpLinks(this.scene);
+
+    this.renderer.setAnimationLoop(this.animate);
+  }
+
+  addStars(scene: THREE.Scene) {
+    const starGeometry = new THREE.SphereGeometry(0.1);
+    this.starSystemsService.getStarSystems()
+      .forEach(starSystem => {
+        const starMaterial = starSystem.name === "Sol" ?
+          new MeshBasicMaterial({color: 0xffff00}) :
+          new MeshBasicMaterial({color: 0xffffff});
+        const starMesh = new THREE.Mesh(starGeometry, starMaterial);
+        starMesh.position.set(
+          starSystem.coordinates.x,
+          starSystem.coordinates.y,
+          starSystem.coordinates.z
+        );
+        scene.add(starMesh);
+      });
+  }
+
+  addJumpLinks(scene: THREE.Scene) {
+    this.starSystemsService.getStarSystems()
+      .flatMap(starSystem => {
+        const origin = new THREE.Vector3(
+          starSystem.coordinates.x,
+          starSystem.coordinates.y,
+          starSystem.coordinates.z
+        );
+
+        return starSystem.jumpLinks
+          .filter(jumpLink => jumpLink.discovered !== null)
+          .map(jumpLink => {
+            return {
+              jumpLevel: jumpLink.jumpLevel,
+              coordinates: this.starSystemsService.getStarSystems()
+                .find(starSystem => starSystem.name === jumpLink.destination)
+                ?.coordinates
+            };
+          })
+          .filter((jumpLink): jumpLink is { jumpLevel: string, coordinates: {x: number, y: number, z: number} } => jumpLink.coordinates !== undefined)
+          .map(jumpLink => {
+            const destination = new THREE.Vector3(
+              jumpLink.coordinates.x,
+              jumpLink.coordinates.y,
+              jumpLink.coordinates.z
+            );
+
+            return {jumpLevel: jumpLink.jumpLevel, origin: origin, destination: destination};
+          });
+      })
+      .forEach(jumpLink => {
+        let lineMaterial;
+        switch (jumpLink.jumpLevel) {
+          case "Alpha":
+            lineMaterial = new THREE.LineBasicMaterial({color: 'red'});
+            break;
+          case "Beta":
+            lineMaterial = new THREE.LineBasicMaterial({color: 'orange'})
+            break;
+          case "Gamma":
+            lineMaterial = new THREE.LineBasicMaterial({color: 'yellow'})
+            break;
+          case "Delta":
+            lineMaterial = new THREE.LineBasicMaterial({color: 'green'})
+            break;
+          case "Epsilon":
+            lineMaterial = new THREE.LineBasicMaterial({color: 'blue'})
+            break;
+        }
+
+        const lineGeometry = new THREE.BufferGeometry()
+          .setFromPoints([jumpLink.origin, jumpLink.destination]);
+
+        const line = new THREE.Line(lineGeometry, lineMaterial);
+        scene.add(line);
+      });
+  }
+
+  getCoordinateLimit() {
+    return Math.max(...this.starSystemsService.getStarSystems()
+      .flatMap(starSystem => {
+        return [starSystem.coordinates.x, starSystem.coordinates.y, starSystem.coordinates.z];
+      }));
+  }
+
+  createScene() {
+    return new THREE.Scene();
+  }
+
+  createCamera(coordinateLimit: number) {
     const aspectRatio = window.innerWidth / window.innerHeight;
 
     const leftLimit = aspectRatio > 1 ? -coordinateLimit * aspectRatio : -coordinateLimit;
@@ -48,69 +132,19 @@ export class StarMapComponent {
     const topLimit = aspectRatio > 1 ? coordinateLimit : coordinateLimit / aspectRatio;
     const bottomLimit = aspectRatio > 1 ? -coordinateLimit : -coordinateLimit / aspectRatio;
 
-    this.camera = new THREE.OrthographicCamera(
-      leftLimit, rightLimit,
-      topLimit, bottomLimit,
-      0.1, coordinateLimit * 2
-    );
-    this.camera.position.z = coordinateLimit;
+    return new THREE.OrthographicCamera(leftLimit, rightLimit, topLimit, bottomLimit, 0.1, 100);
+  }
 
-    const stars = starSystemsService.getStarSystems()
-      .map(starSystem => ({name: starSystem.name, coordinates: starSystem.coordinates}));
+  createRenderer() {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-    let jumpLinks = starSystemsService.getStarSystems()
-      .flatMap(starSystem => {
-        const origin = starSystem.coordinates;
-        return starSystem.jumpLinks
-          .filter(jumpLink => jumpLink.discovered !== null)
-          .map(jumpLink => {
-          let destination = stars.find(star => star.name === jumpLink.destination)!.coordinates;
-          return {origin: origin, destination: destination, jumpLevel: jumpLink.jumpLevel}
-        });
-      })
-      .filter((val)  => val.destination !== undefined);
+    const container = document.getElementById("divCanvas") ?? document.body;
+    container.appendChild(renderer.domElement);
+    return renderer;
+  }
 
-    stars.forEach(star => {
-      const starGeometry = new THREE.SphereGeometry(0.1);
-      const starMaterial = star.name === "Sol" ?
-        new MeshBasicMaterial({color: 0xffff00}) :
-        new MeshBasicMaterial({color: 0xffffff});
-      const starMesh = new THREE.Mesh(starGeometry, starMaterial);
-      starMesh.position.set(star.coordinates.x, star.coordinates.y, star.coordinates.z);
-      this.scene.add(starMesh);
-    });
-
-    jumpLinks.forEach(jumpLink => {
-      let lineMaterial;
-      switch (jumpLink.jumpLevel) {
-        case "Alpha":
-          lineMaterial = new THREE.LineBasicMaterial({color: 'red'});
-          break;
-        case "Beta":
-          lineMaterial = new THREE.LineBasicMaterial({color: 'orange'})
-          break;
-        case "Gamma":
-          lineMaterial = new THREE.LineBasicMaterial({color: 'yellow'})
-          break;
-        case "Delta":
-          lineMaterial = new THREE.LineBasicMaterial({color: 'green'})
-          break;
-        case "Epsilon":
-          lineMaterial = new THREE.LineBasicMaterial({color: 'blue'})
-          break;
-      }
-
-      const lineGeometry = new THREE.BufferGeometry()
-        .setFromPoints([
-          new THREE.Vector3(jumpLink.origin.x, jumpLink.origin.y, jumpLink.origin.z),
-          new THREE.Vector3(jumpLink.destination.x, jumpLink.destination.y, jumpLink.destination.z)
-        ]);
-
-      const link = new THREE.Line(lineGeometry, lineMaterial);
-      this.scene.add(link);
-    });
-
-    this.controls = thiAs.createControls(this.camera);
-    this.renderer.setAnimationLoop(this.animate);
+  createControls(camera: Camera, renderer: WebGLRenderer) {
+    return new TrackballControls(camera, renderer.domElement);
   }
 }
